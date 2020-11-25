a0fa6bf9a6b320f22d714194ff652a29cc7a805f
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-25 14:38:58 +0100

    FIRST CODE IMPROVE

--- js/script.js
+++ js/script.js
@@ -9,21 +9,20 @@
 const boloflix = new Vue ({
     el: "#boloflix",
     data: {
-        // Inizializzo search nei data  
-        // Dinamicità = binding con la input text searchbar (V-MODEL)
+        // Inizializzo search nei data e bindo il suo valore all'input text per la ricerca dei titoli (V-MODEL)
         search: '',
-        // Array FILM da popolare con i dati da API
+        // Array FILM da popolare con i dati da API + filtrati dalla ricerca dell'utente
         films: [],
-        // Array FILM da popolare con i dati da API
+        // Array FILM da popolare con i dati da API + filtrati dalla ricerca dell'utente
         series: []
     },
     methods: {
-        // Ricerca bindata con @keyUp al tasto invio e con il @click al button accanto alla barra di ricerca all'interno della quale inserisco i due metodi per cercare FILM e SERIE TV, in questo modo all'invio da parte dell'utente attraverso una delle due modalità a sua disposizione, grazie al lavoro delle due funzioni, visualizzo a schermo i risultati che matchano tra   richiesta utente <---> elementi presenti nell'array
+        // Ricerca bindata con @keyUp al tasto invio e con il @click al button accanto alla barra di ricerca all'interno della quale inserisco i due metodi per cercare FILM e SERIE TV, in questo modo all'invio da parte dell'utente attraverso una delle due modalità a sua disposizione, grazie al lavoro delle due funzioni, visualizzo a schermo i risultati che matchano tra     RICHIESTA UTENTE       <--->        ELEMENTI NELL'ARRAY
         itemSearchByTheUser() {
             this.findFilms();
             this.findTvSeries();
           },
-        // FindFilms & FindTvSeries funzionano allo stesso modo: costruisco aiutandomi con i params la mia chiamata API, dopodichè, se questa va a buon fine, utilizzo lo spread operator perchè mi consente di creare ad ogni nuova richiesta un array che si integra (ed in questo caso va a popolare) l'array Films & Series che ho inizializzato nei data.
+        // FindFilms & FindTvSeries funzionano allo stesso modo: costruisco aiutandomi con i params la mia chiamata API, dopodichè, se questa va a buon fine, utilizzo lo spread operator perchè mi consente di creare ad ogni nuova richiesta un array che popola dinamicamente gli array Films & Series che ho inizializzato nei data.
         findFilms() {
             if (this.search) {
                 axios.get('https://api.themoviedb.org/3/search/movie', {
