35dd70ff8c7f21e69fb3811b5ca9a01024858990
Cristian Gardosi <cristian.gardosi97@gmail.com>
2020-11-25 12:15:58 +0100

    API W/PARAMS STRUCTURE AND @KEYUP @CLICK EVENTS

--- js/script.js
+++ js/script.js
@@ -9,18 +9,22 @@
 const boloflix = new Vue ({
     el: "#boloflix",
     data: {
-
+        // Inizializzo search nei data  
+        // Dinamicità = binding con la input text searchbar (V-MODEL)
+        search: '',
     },
     created() {
         // Targhettizziamo la API
         axios.get('https://api.themoviedb.org/3/search/movie', {
             params: {
-                api_key: 'd941bb4acf4132519ffe2318bdce1523'
+                api_key: 'd941bb4acf4132519ffe2318bdce1523',
+                query: 'blue shining',
+                language: 'it-IT'
             }
         })
         // Situazione di successo nella chiamata API
         .then( response => {
-            console.log(response.data)
+            console.log(response.data.results)
         })
         // Situazione di errore nella chiamata API
         .catch( error => {
@@ -28,7 +32,10 @@ const boloflix = new Vue ({
         });
     },
     methods: {
-
+        // Ricerca bindata con @keyUp al tasto invio e con il @click al button accanto alla barra di ricerca
+        startSearch(){
+            console.log(this.search.trim().toLowerCase());
+        }
     }
 
 
